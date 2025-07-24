a43d3b30494fe91ccb806022e9082878cf31d3d0
Zhuoran_Li <m15677310992@163.com>
2025-07-24 23:35:34 +0800

    mean and trans data

--- prob_1.py
+++ prob_1.py
@@ -17,9 +17,27 @@ def extract_last_hour_data(file_name):
                 header=True, index=False)
     return data
 
-def transpose_data(data):
-    pass
-    
+
+def transpose_data_and_mean(data):
+    # 将1142A到1150A、1151A到1222A、1223A到1269A的数据按列求平均值，然后重新整合数据，再转置
+    data_1142A_1150A = data.iloc[:, 3:8]
+    data_1151A_1222A = data.iloc[:, 9:56]
+    data_1223A_1269A = data.iloc[:, 57:]
+    data_1142A_1150A_mean = data_1142A_1150A.mean(axis=1)
+    data_1151A_1222A_mean = data_1151A_1222A.mean(axis=1)
+    data_1223A_1269A_mean = data_1223A_1269A.mean(axis=1)
+    data_mean = pd.concat(
+        [data_1142A_1150A_mean, data_1151A_1222A_mean, data_1223A_1269A_mean], axis=1)
+    data_mean.columns = [
+        '1142A_1150A(ShangHai)', '1151A_1222A(JiangSu)', '1223A_1269A(ZheJiang)']
+    # 提取第2/4/6/8/10/13/15行
+    data_mean = data_mean.iloc[[0, 2, 4, 6, 8, 11, 13], :]
+    data_mean.index = ['aqi', 'pm2.5', 'pm10', 'so2', 'no2', 'co', 'o3']
+    data_mean = data_mean.T
+    print(f"\n")
+    print("转置并求平均值")
+    print(data_mean)
+    return data_mean
 
 
 if __name__ == "__main__":
@@ -33,3 +51,7 @@ if __name__ == "__main__":
     print(data)
     extract_last_hour_data(
         "res/china_sites_20250215_1141A_1269A.csv")
+    df = pd.read_csv("res/china_sites_20250215_1141A_1269A_last_hour.csv")
+    df = transpose_data_and_mean(df)
+    df.to_csv("res/china_sites_20250215_1141A_1269A_last_hour_mean_and_transpose.csv",
+              header=True, index=True)
